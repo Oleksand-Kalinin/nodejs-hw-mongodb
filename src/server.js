a995231ca55e760ba9f633989237170bf16298ef
Oleksandr-Kalinin <work.kalinin.o.v@gmail.com>
2024-10-01 14:14:02 +0300

    add routes

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { env } from './utils/env.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
 
 
 
@@ -14,13 +15,37 @@ export const setupServer = () => {
     app.use(express.json());
     app.use(cors());
 
-    app.use('*', pino({
+    app.use(pino({
         transport: {
             target: 'pino-pretty',
         },
     }));
 
-    app.get('', (req, res) => {
+    app.get('/contacts', async (req, res) => {
+        const contacts = await getAllContacts();
+        res.status(200).json({
+            data: contacts,
+        })
+    });
+
+    app.get('/contacts/:contactId', async (req, res) => {
+        const { contactId } = req.params;
+        const contact = await getContactById(contactId);
+
+        if (!contact) {
+            res.status(404).json({
+                message: "Contact not found"
+            })
+            return;
+        }
+
+        res.status(200).json({
+            data: contact,
+        })
+    })
+
+
+    app.get('/', (req, res) => {
         res.json({ message: 'Server is enable' });
     });
 
